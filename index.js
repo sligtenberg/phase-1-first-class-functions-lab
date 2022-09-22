8b6ebc02f8ca279cc98760afcbb24a4cc72d0a7b
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-22 00:37:40 -0400

    commit tree a2afff116e21eb6cb5ccbc12b38c908f0fbeb312

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// in this lab, i started with long versions of functions, and translated them into arrow functions as practice
+
+const returnFirstTwoDrivers = arrayOfDrivers => arrayOfDrivers.slice(0, 2);
+
+const returnLastTwoDrivers = arrayOfDrivers => arrayOfDrivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = multiplier => fare => fare * multiplier; // sleek!
+
+/* below is the non-arrow function version of the above function so i can read it
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return multiplier * fare;
+    }
+} */
+
+const fareDoubler = fare => createFareMultiplier(2)(fare);
+
+/* below is the non-arrow function version of the above function so i can read it
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+} */
+
+const fareTripler = fare => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFuncs) => returnFuncs(arrayOfDrivers);
